653290075423caaad63558f95a6596957ce4f71c
andulkalam6 <akad2003k@gmail.com>
2024-09-05 00:15:47 +0530

    First commit

---
+++ cart.js
@@ -0,0 +1,15 @@
+{
+    "token": "bb3bf81353d0fbea42326be721714f17",
+    "note": null,
+    "attributes": {},
+    "original_total_price": 0,
+    "total_price": 0,
+    "total_discount": 0,
+    "total_weight": 0.0,
+    "item_count": 0,
+    "items": [],
+    "requires_shipping": false,
+    "currency": "USD",
+    "items_subtotal_price": 0,
+    "cart_level_discount_applications": []
+}
